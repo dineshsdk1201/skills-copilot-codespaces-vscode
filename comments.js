0b7f6d337eb9bd049443e3e5828dbeb0750420ac
dineshsdk1201 <159545728+dineshsdk1201@users.noreply.github.com>
2024-02-14 06:16:03 +0000

    Copilot new commit

--- comments.js
+++ comments.js
@@ -1,21 +1,42 @@
-// Create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var path = require('path');
-var commentFile = 'comment.json';
-var commentData = require('./' + commentFile);
-var port = 8080;
+// Create web server with express, and handle requests with comments.js
+// Comments.js will fetch the comments from the database and return them as a JSON object
+// Comments.js will also handle adding new comments to the database
+// This file is a module
+// It exports a function that takes the database as an argument and returns an express app
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+module.exports = function(db) {
+    var express = require('express');
+    var comments = express.Router();
+    var bodyParser = require('body-parser');
+    comments.use(bodyParser.json());
 
-app.get('/comment', function(req, res) {
-  res.json(commentData);
-});
+    // Fetch all comments
+    comments.get('/', function(req, res) {
+        db.all('SELECT * FROM comments', function(err, rows) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(rows);
+            }
+        });
+    });
 
-app.post('/comment', function(req, res) {
-  var newComment = {
-    id: Date.now(),
+    // Add a new comment
+    comments.post('/', function(req, res) {
+        var name = req.body.name;
+        var comment = req.body.comment;
+        if (name && comment) {
+            db.run('INSERT INTO comments (name, comment) VALUES (?, ?)', [name, comment], function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(201).send('Comment added');
+                }
+            });
+        } else {
+            res.status(400).send('Missing name or comment');
+        }
+    });
+
+    return comments;
+};
